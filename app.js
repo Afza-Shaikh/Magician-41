f4948f601ec8db77c27f0ebf9376f79a6682b6a2
afza shaikh <shaikhafza961@gmail.com>
2024-09-16 22:51:56 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+"use strict";
+let magician_list = ['David Copperfield', 'Criss Angel', 'David Balaine', 'penn & Teller'];
+for (let i = 0; i < magician_list.length; i++) {
+    function call_magician(list) {
+        console.log(list);
+    }
+    console.log(magician_list);
+}
---
+++ app.ts
@@ -0,0 +1,7 @@
+let magician_list: string[] = ['David Copperfield','Criss Angel','David Balaine','penn & Teller']
+for(let i=0; i<magician_list.length; i++){
+    function call_magician(list:string,){
+        console.log(list)
+    }
+    console.log(magician_list)
+}
